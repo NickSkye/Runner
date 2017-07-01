e02f97cea490e807f591637a24bbd9e55c4f2c9a
NickSkye <nickskye7@gmail.com>
2017-06-30 20:52:44 -0700

    Added new song

--- Beetle/MusicHelper.swift
+++ Beetle/MusicHelper.swift
@@ -4,16 +4,16 @@ class MusicHelper {
     static let sharedHelper = MusicHelper()
     var audioPlayer: AVAudioPlayer?
     var playing = false;
-    var song = "gno"
+    //var song = "gno"
     func playBackgroundMusic() {
-         let randomNum = Int(arc4random_uniform(UInt32(6)))
-        print("random \(randomNum)")
-        print("random \(randomNum % 2)")
-        if randomNum < 3  {
+       //  let randomNum = Int(arc4random_uniform(UInt32(6)))
+       // print("random \(randomNum)")
+       // print("random \(randomNum % 2)")
+        /* if randomNum < 3  {
             song = "Arabesque"
-        }
+        } */
         
-        let aSound = URL(fileURLWithPath: Bundle.main.path(forResource: song, ofType: "mp3")!)
+        let aSound = URL(fileURLWithPath: Bundle.main.path(forResource: "DooblyDoo", ofType: "mp3")!)
         do {
             audioPlayer = try AVAudioPlayer(contentsOf:aSound)
             audioPlayer!.numberOfLoops = -1
